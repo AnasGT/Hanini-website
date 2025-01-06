d9b17ca735388ad85094aa0fe6fc775318ae71dc
AnasGT <anasalasmar10@gmail.com>
2025-01-06 17:48:48 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container'>
+        <header>
+        <img className='logo' src='/images/logo.png' alt="Logo" />
+          <h1>Hanini</h1>
+        </header>
+        <section>
+          <div className='text'>
+            <h3>FLUTTER MOBILE APP</h3>
+          <p>
+            Hanini App offers a seamless experience for discovering and booking services. With a sleek design and intuitive interface, it connects users with reliable providers, ensuring convenience and satisfaction. Download the app today to simplify your service needs.
+          </p>
+          <a href="https://uca96ec985b4fbca8fae64005e0a.dl.dropboxusercontent.com/cd/0/get/ChozldGuAGUJ6IR5N0QOuYiP5TfY435dRlsCpkZcSFTq6mcFKhPfGJV4tCKmombFJoMq21E6QQ4sDzQSnMtD_z6OLriZZrakA9RIfCTeZkECLJqMqif7JtCMcmkZxzu2HRu7A6iSyvS-mB5XkgFLDMgT2KVkRXTYnj8w5FfI7zAHnw/file?_download_id=1042724845033478976373805150202760739415522474377504493755128501&_log_download_success=1&_notify_domain=www.dropbox.com&dl=1"><button>Download</button></a>
+          </div>
+
+          <img className='phone' src="/images/phone_1-removebg.png" alt="Phone Preview" />
+        </section>
+      </div>
+    </div>
+  );
+}
+
+export default App;
